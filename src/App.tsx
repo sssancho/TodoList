6c411b0a25a11efe6ddf175a1f0dd803a23e2f3b
sssancho <blogersanjar@gmail.com>
2023-03-03 07:40:42 +0100

    Add New component TodoList and Make new Props and Props types

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import './App.css';
+import { TaskType, TodoList } from './TodoList';
 
 function App() {
-  debugger;
+
+  let task1: Array<TaskType> = [
+    {id: 1, title: "Css", isDone: true},
+    {id: 2, title: "React", isDone: true},
+    {id: 3, title: "Redux", isDone: false},
+
+  ]
+
+  let task2: Array<TaskType> = [
+    {id: 1, title: "Doctor Strange", isDone: false},
+    {id: 2, title: "Fast Furios", isDone: true},
+    {id: 3, title: "Sherlock", isDone: true},
+
+  ]
+
+
   return (
     <div className="App">
-        <TodoList />
-        <TodoList />
+        <TodoList title="What to Learn" tasks={task1}/>
+        <TodoList title="Movies" tasks={task2}/>
     </div>
   );
-}
-
-function TodoList(){
-  debugger;
-  return (
-    <div>
-      <h1>Learn and Be Better</h1>
-      <h1>And Get a Dream Job FullStack Developer</h1>
-    </div>
-  )
 }
 
 export default App;
