32869400d79591434d79220640aae6a56e347a1e
sssancho <blogersanjar@gmail.com>
2023-03-09 10:28:28 +0100

    Add New function remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 import { TaskType, TodoList } from './TodoList';
 
+
 function App() {
 
-  let task1: Array<TaskType> = [
-    {id: 1, title: "Css", isDone: true},
-    {id: 2, title: "React", isDone: true},
-    {id: 3, title: "Redux", isDone: false},
+  let initTasks: Array<TaskType> = [
+    {id: 1, title: "Html&Css", isDone: true},
+    {id: 2, title: "JavaScript", isDone: true},
+    {id: 3, title: "React", isDone: false},
+    {id: 4, title: "Redux", isDone: false},
 
   ]
 
-  let task2: Array<TaskType> = [
-    {id: 1, title: "Doctor Strange", isDone: false},
-    {id: 2, title: "Fast Furios", isDone: true},
-    {id: 3, title: "Sherlock", isDone: true},
+  let arr = useState(initTasks)
+  let tasks = arr[0]
+  let setTasks = arr[1]
 
-  ]
-
+  function removeTasks(id: number){
+    let filteredTasks = tasks.filter( (t) => t.id !== id)
+    setTasks(filteredTasks)
+  }
 
   return (
     <div className="App">
-        <TodoList title="What to Learn" tasks={task1}/>
-        <TodoList title="Movies" tasks={task2}/>
+        <TodoList 
+            title="What to Learn" 
+            tasks={tasks}
+            removetask={removeTasks}  
+          />
     </div>
   );
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -7,22 +7,27 @@ export type TaskType = {
 
 type PropsType = {
   title: string,
-  tasks: Array<TaskType>
+  tasks: Array<TaskType>,
+  removetask: Function
 }
 
 export function TodoList(props: PropsType) {
-
   return (
     <div>
       <h3>{props.title}</h3>
-      <div>
+      <div className="inputHandler">
         <input />
         <button>+</button>
       </div>
       <ul>
-        <li><input type="checkbox" checked={props.tasks[0].isDone} /><span>{props.tasks[0].title}</span></li>
-        <li><input type="checkbox" checked={props.tasks[1].isDone} /><span>{props.tasks[1].title}</span></li>
-        <li><input type="checkbox" checked={props.tasks[2].isDone} /><span>{props.tasks[2].title}</span></li>
+        {
+          props.tasks.map( (t) => {
+            return  <li><input type="checkbox" checked={t.isDone} />
+                    <span>{t.title}</span>
+                    <button onClick={() => {props.removetask(t.id)}}>X</button>
+                </li>
+          })
+        }
       </ul>
       <div>
           <button>All</button>
